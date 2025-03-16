b59f8133516fdf81f489f527d1a0f77e99f41ee0
Volodymyr199606 <your-email@example.com>
2025-03-15 20:02:37 -0700

    update main

--- src/main/java/org/com/app/Main.java
+++ src/main/java/org/com/app/Main.java
@@ -1,8 +1,11 @@
 
 package org.com.app;
 
+import org.com.database.DatabaseHelper;
+
 public class Main {
     public static void main(String[] args) {
+        DatabaseHelper.createTable();
 
         }
     }
