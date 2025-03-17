93867cdcb41f5ed73304dbc718d9174075a72fee
Volodymyr199606 <your-email@example.com>
2025-03-17 16:05:04 -0700

    implementing logic

--- src/main/java/org/com/app/Main.java
+++ src/main/java/org/com/app/Main.java
@@ -1,11 +1,13 @@
 
 package org.com.app;
 
+import org.com.controller.UserController;
 import org.com.database.DatabaseHelper;
 
 public class Main {
     public static void main(String[] args) {
         DatabaseHelper.createTable();
+        UserController.startApplication();
 
-        }
     }
+}
--- src/main/java/org/com/controller/UserController.java
+++ src/main/java/org/com/controller/UserController.java
@@ -1,4 +1,29 @@
 package org.com.controller;
 
+import org.com.service.UserService;
+import org.com.services.*;
+import java.util.Scanner;
+
 public class UserController {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final UserService userService = new UserService();
+
+    private static final String NAME_REGEX = "^[A-Za-z]+$";
+    private static final String AGE_REGEX = "^\\d+$";
+    private static final String GENDER_REGEX = "^(male|female)$";
+    private static final String ACTIVITY_REGEX = "^(sedentary|active)$";
+
+
+    private static final Loggable[] services = {
+            new ExerciseService(),
+            new WaterService(),
+            new FruitService(),
+            new VegetableService()
+    };
+
+    public static void startApplication() {
+
+    }
 }
+
+
